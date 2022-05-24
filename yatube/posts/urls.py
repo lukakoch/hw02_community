5d0d9a2278682be62eb3a359a7a98d553612a73b
kocher <lukakoh@yandex.ru>
2022-05-24 12:58:13 +0300

    вроде все

---
+++ yatube/posts/urls.py
@@ -0,0 +1,12 @@
+# posts/urls.py
+from argparse import Namespace
+from django.urls import path
+
+from . import views
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+
+    path('group/<slug:slug>/', views.group_posts, name = 'group_list'),
+]
