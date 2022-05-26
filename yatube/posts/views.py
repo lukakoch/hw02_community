df64c42f8d140ac342e2be85b24561f61f87f82a
kocher <lukakoh@yandex.ru>
2022-05-26 19:50:50 +0300

    все исправлено

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
+from django.conf import settings
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:settings.MAX_POSTS]
     context = {
         'posts': posts,
         'text_title': 'Последние обновления на сайте',
@@ -14,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:settings.MAX_POSTS]
     context = {
         'group': group,
         'posts': posts,
