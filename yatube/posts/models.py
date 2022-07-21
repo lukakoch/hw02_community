cc7955a5e74de4784e835841a2a2761b878e7a08
kocher <lukakoh@yandex.ru>
2022-07-22 00:58:02 +0300

    //

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='PostAdmin'
+        related_name='AuthorName'
     )
     group = models.ForeignKey(
         Group,
